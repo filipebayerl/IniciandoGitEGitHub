577244a240a6153b67fd5f4ea71644ea41979b72
Filipe Bayerl <filipebayerl@gmail.com>
2025-07-02 16:35:07 -0300

    primeiros exercícios array
    testando array

---
+++ site-exemplo/testes-array.js
@@ -0,0 +1,9 @@
+// push: Adiciona um elemento no final do array e devolve o novo tamanho do array
+let tamanho = arr.push("Boromir")
+console.log(arr)
+console.log(tamanho)
+
+// unshift: Adiciona um elemento no começo do array e devolve o novo tamanho do array
+tamanho = arr.unshift("Boromir")
+console.log(arr)
+console.log(tamanho)
